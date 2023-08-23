1fdca20f0e7830b8ae6b8a4c74acee4e706818ab
quyetlv12 <lequyet840500@gmail.com>
2023-08-23 16:56:08 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Saved from './screens/saved';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Tab = createBottomTabNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+     <Tab.Navigator
+     screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'ios-home-sharp'
+            : 'ios-home-outline';
+        } else if (route.name === 'Saved') {
+          iconName = focused ? 'ios-save' : 'ios-save-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
+     >
+      <Tab.Screen name="Home" component={Home} options={{ tabBarBadge: 3 }}/>
+      <Tab.Screen name="Saved" component={Saved} options={{ tabBarBadge: 10 }}/>
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App
